445653d7d27d8aa6ceda386d19a073e5d104d55a
LatashaWanjira <latasha.wanjira@gmail.com>
2017-02-02 15:44:16 +0300

    styling added to drivers route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,14 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return this.store.findAll('driver');
+        return this.store.findAll('drivers');
     },
 
     actions: {
-        // saveDriver(params) {
-        //     var newDriver = this.store.createRecord('drivers', params);
-        //     newDriver.save();
-        //     this.transitionTo('for-drivers');
-        // }
     }
 });
